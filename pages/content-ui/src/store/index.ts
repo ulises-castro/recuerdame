8d05e6a826705b16f79191ff3fd229be79ee6529
Ulises Castro <17864276+ulises-castro@users.noreply.github.com>
2024-10-01 18:26:10 -0600

    feat: add data storage persistency for zustand using chrome.storage api

--- pages/content-ui/src/store/index.ts
+++ pages/content-ui/src/store/index.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { createJSONStorage, persist, StateStorage } from 'zustand/middleware'
 import createSelectors from './createSelectors'
 
 import { nanoid } from 'nanoid'
@@ -20,32 +20,33 @@ const createPageHighlight = (url: string, range: Range): PageHighlight => {
   }
 }
 
-const userStoreBase = create<UserState>((set) => ({
-  savedHighlights: null,
-  addHighlightedText: (url, range) =>
-    set((state) => {
-      const updatedHighlights = [...state.savedHighlights ?? []]
-      updatedHighlights.push(createPageHighlight(url, range))
-
-      return { savedHighlights: updatedHighlights }
+const extensionStorage: StateStorage = {
+  getItem: async (name) =>
+    new Promise((resolve) => {
+      chrome.storage.local.get([name], (result) => {
+        resolve(result[name])
+      })
     }),
-}))
+  setItem: async (name, value) => chrome.storage.local.set({ [name]: value }),
+  removeItem: async (name) => chrome.storage.local.remove(name),
+}
 
-// const userStoreBase = create<UserState>(persist(
-//   (set) => ({
-//     savedHighlights: null,
-//     addHighlightedText: (url, range) =>
-//       set((state) => {
-//         const updatedHighlights = [...state.savedHighlights ?? []]
-//         updatedHighlights.push(createPageHighlight(url, range))
-//
-//         return { savedHighlights: updatedHighlights }
-//       })
-//   }),
-//   {
-//     name: 'highlighted-store',
-//   }
-// ))
+const userStoreBase = create<UserState>(persist(
+  (set) => ({
+    savedHighlights: null,
+    addHighlightedText: (url, range) =>
+      set((state) => {
+        const updatedHighlights = [...state.savedHighlights ?? []]
+        updatedHighlights.push(createPageHighlight(url, range))
+
+        return { savedHighlights: updatedHighlights }
+      })
+  }),
+  {
+    name: 'highlighted-date-store',
+    storage: createJSONStorage(() => extensionStorage)
+  }
+))
 
 export const useUserStore = createSelectors(userStoreBase)
 
